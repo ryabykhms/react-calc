fb1516ad31612d909eccdcf3f25f9e2f298177af
upstrocker <upstrocker@gmail.com>
2020-05-16 13:35:59 +0300

    Button panel style changed

--- src/Panel.js
+++ src/Panel.js
@@ -2,16 +2,16 @@ import Button from "./Button";
 import React from "react";
 
 function Panel(props) {
-  const operations = props.operations;
+  const operationsList = props.operations;
   const onButtonClick = props.onButtonClick;
 
-  const operationButtons = operations.map((operations, id) => {
+  const operationButtons = operationsList.map((operations, id) => {
     return (
       <div className="calc-panel__row" key={id}>
         {
           operations.map((operation, idx) => {
             return (
-              <Button key={idx} operation={operation} onButtonClick={onButtonClick} />
+              <Button key={idx} operation={operation} onButtonClick={onButtonClick}/>
             )
           })
         }
